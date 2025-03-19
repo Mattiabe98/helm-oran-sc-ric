36a6b406fe6406dc231b627dcef73864f9ca6acb
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-03-19 13:47:55 +0100

    Update xAppBase.py

--- charts/python_xapp_runner/lib/xAppBase.py
+++ charts/python_xapp_runner/lib/xAppBase.py
@@ -25,7 +25,7 @@ class xAppBase(object):
     def __init__(self, config=None, http_server_port=8090, rmr_port=4560, rmr_flags=0x00):
         super(xAppBase, self).__init__()
         # Default Config
-        self.xAPP_IP = "10.0.2.20"
+        self.xAPP_IP = "xapp-service"
         self.MY_HTTP_SERVER_ADDRESS = "0.0.0.0"     # bind to all interfaces
         self.MY_HTTP_SERVER_PORT = http_server_port # web server listen port
         self.MY_RMR_PORT = rmr_port
