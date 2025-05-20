2e1d446497c3c1025bb8848f1c27a12be31520b6
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-05-20 14:07:54 +0200

    Update e2sm_rc_module.py

--- charts/python_xapp_runner/lib/e2sm_rc_module.py
+++ charts/python_xapp_runner/lib/e2sm_rc_module.py
@@ -39,7 +39,7 @@ class e2sm_rc_module(object):
         return payload
 
     def send_control_request_style_2_action_6(self, e2_node_id, ue_id, min_prb_ratio, max_prb_ratio, dedicated_prb_ratio, ack_request=1):
-        plmn_string = "00101"
+        plmn_string = "99992"
         print(plmn_string)
         sst = 1
         sd = 1
@@ -83,9 +83,9 @@ class e2sm_rc_module(object):
                                                         #>>>>>S-NSSAI, STRUCTURE
                                                         {'ranParameter-ID': 8, 'ranParameter-valueType': ('ranP-Choice-Structure', {'ranParameter-Structure': {'sequence-of-ranParameters': [
                                                                 #>>>>>>SST, ELEMENT
-                                                                {'ranParameter-ID': 9, 'ranParameter-valueType': ('ranP-Choice-ElementFalse', {'ranParameter-value': ('valueOctS', sst)})},
+                                                                {'ranParameter-ID': 9, 'ranParameter-valueType': ('ranP-Choice-ElementFalse', {'ranParameter-value': ('valueOctS', sst)})}
                                                                 #>>>>>>SD, ELEMENT
-                                                                {'ranParameter-ID': 10, 'ranParameter-valueType': ('ranP-Choice-ElementFalse', {'ranParameter-value': ('valueOctS', sd)})}]
+                                                                # {'ranParameter-ID': 10, 'ranParameter-valueType': ('ranP-Choice-ElementFalse', {'ranParameter-value': ('valueOctS', sd)})}]
                                                             }})}]}})}]}]}})}]}})},
                                             #>>Min PRB Policy Ratio, ELEMENT
                                             {'ranParameter-ID': 11, 'ranParameter-valueType': ('ranP-Choice-ElementFalse', {'ranParameter-value': ('valueInt', min_prb_ratio)})},
