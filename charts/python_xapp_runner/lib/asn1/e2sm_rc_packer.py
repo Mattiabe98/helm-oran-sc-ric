054a981bd4d9df36627341b6676e61fdd8dcdc7b
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-05-20 14:32:49 +0200

    Update e2sm_rc_packer.py

--- charts/python_xapp_runner/lib/asn1/e2sm_rc_packer.py
+++ charts/python_xapp_runner/lib/asn1/e2sm_rc_packer.py
@@ -10,9 +10,11 @@ class e2sm_rc_packer(object):
 
     def pack_ric_control_header_f1(self, style_type, control_action_id, ue_id_tuple):
         control_header = {'ric-controlHeader-formats': ('controlHeader-Format1', {'ueID': ue_id_tuple, 'ric-Style-Type': style_type, 'ric-ControlAction-ID': control_action_id})}
+        print(control_header)
         control_header = self.asn1_compiler.encode('E2SM-RC-ControlHeader', control_header)
         return control_header
 
     def pack_ric_control_msg(self, control_msg_dict):
+        print(control_msg_dict)
         control_header = self.asn1_compiler.encode('E2SM-RC-ControlMessage', control_msg_dict)
         return control_header
