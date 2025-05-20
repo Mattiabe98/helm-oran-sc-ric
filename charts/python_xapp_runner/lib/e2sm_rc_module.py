e00b6e968dfb3f7532060caf7790ac1179c7cb80
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-05-20 12:38:35 +0200

    Update e2sm_rc_module.py

--- charts/python_xapp_runner/lib/e2sm_rc_module.py
+++ charts/python_xapp_runner/lib/e2sm_rc_module.py
@@ -38,7 +38,7 @@ class e2sm_rc_module(object):
         payload = bytes(hex_num for hex_num in msg)
         return payload
 
-    def send_control_request_style_2_action_6(self, e2_node_id, ue_id, min_prb_ratio, max_prb_ratio, plmn_string, dedicated_prb_ratio, ack_request=1):
+    def send_control_request_style_2_action_6(self, e2_node_id, ue_id, plmn_string, min_prb_ratio, max_prb_ratio, , dedicated_prb_ratio, ack_request=1):
         plmn_string = str(plmn_string)
         sst = 1
         sd = 0
